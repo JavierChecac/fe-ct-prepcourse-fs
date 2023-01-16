d4bcfa84b5a7d76b9228dc704be1cfa0e6d08445
Javier Checa <ulises7.jc@gmail.com>
2023-01-16 10:02:38 -0500

    avance resueltp de ejercicio js1

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,32 +5,45 @@ function sonIguales(x, y) {
    // Retorna True si "x" e "y" son iguales.
    // De lo contrario, retorna False.
    // Tu código:
+   return Boolean(x==y);
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna True si los dos strings tienen la misma longitud.
    // De lo contrario, retorna False.
    // Tu código:
+  return Boolean(str1.length === str2.length);
+  
+  
 }
 
 function menosQueNoventa(num) {
    // Retorna True si el argumento "num" es menor que noventa.
    // De lo contrario, retorna False.
    // Tu código:
+   return Boolean(num<90);
 }
 
 function mayorQueCincuenta(num) {
    // Retorna True si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna False.
    // Tu código:
+   return Boolean(num>50);
+   
 }
 
 function esPar(num) {
    // Retorna True si "num" es par.
    // De lo contrario, retorna False.
    // Tu código:
-}
+ Boolean (num%2===0);
+return(true);
+Boolean(num%2===1);
+return(false);
 
+}
+ 
+ 
 function esImpar(num) {
    // Retorna True si "num" es impar.
    // De lo contrario, retorna False.

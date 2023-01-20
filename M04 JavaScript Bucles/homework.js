abdc0bce54060630237a56b9b986cdd52f832881
javier checa <ulises7.jc@gmail.com>
2023-01-20 14:41:49 -0500

    avance de ejercicio bucles

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -125,7 +125,12 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
- 
+   if(num % 15 === 0) return 'fizzbuzz';
+   if(num % 3 === 0) return 'fizz';
+   if(num % 5 === 0) return 'buzz';
+   return false;
+ }
+
 function operadoresLogicos(num1, num2, num3) {
    // La función recibe tres números distintos.
    // Si num1 es mayor a num2 y a num3, y además es positivo, retornar ---> "Numero 1 es mayor y positivo".
@@ -134,7 +139,23 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-}
+   if(num1 < 0 || num2 < 0 || num3 < 0) {
+      return "Hay negativos";
+    }
+    else if(num1 === 0 || num2 === 0 || num3 === 0) {
+      return "Error";
+    }
+    else if(num1 > 0 && num1 > num2 && num1 > num3) {
+      return "Número 1 es mayor y positivo";
+    }
+    else if(num3 > num1 && num3 > num2) {
+      return num3 + 1;
+    }
+    else {
+      return false;
+    }
+  }
+   
 
 function esPrimo(num) {
    // Retornar True si "num" es primo.
@@ -143,7 +164,16 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
